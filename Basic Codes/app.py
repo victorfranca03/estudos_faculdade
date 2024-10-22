e7d138d543baf5c9a3514b1a82800f0b31299cd6
victorfranca03 <ga.victorh@gmail.com>
2024-10-21 21:35:52 -0300

    Primeiros codigos passado nas aulas da faculdade do primeiro modulo

---
+++ Basic Codes/app.py
@@ -0,0 +1,43 @@
+# Codigo feito apenas com intuito de saber a Ideade e conhecer um pouco sobre o Python
+
+
+print("Idade do aluno")
+
+
+ano_nasci = 2003
+ano_atual = 2024
+
+
+idade = ano_atual - ano_nasci
+
+print(idade)
+
+
+
+print("Calculos: ")
+
+
+# Codigos básicos de operadores matemáticos
+
+soma = 1 + 3
+print (soma)
+
+
+subtracao = 4 -1 
+print (subtracao)
+
+multiplicacao = 2 * 99
+print (multiplicacao)
+
+
+divisao = 6 / 2 
+print (divisao)
+
+potencia = 2**3
+print(potencia)
+
+resto = 10 % 100
+print (resto)
+
+parteInteira = 10 // 3
+print(parteInteira)
